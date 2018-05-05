9764d32bb8ed2f7e222eb6c38aa0ce1924bc10d1
Toreilly317 <Taylor@TaylorOReilly.com>
2018-05-05 09:42:26 -0400

    add registration route

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-const express = require("express");
-const mongoose = require("mongoose");
 require("dotenv").config();
 
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
-
 const app = express();
+//body parser middleware
 
-//db config
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//DB Config
 const db = process.env.MONGO_URI;
 
-//CONNECT TO MONGO
+//Conect to mongoDB
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDB Connected"))
+  .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
+app.get("/", (req, res) => res.send("Hello world!"));
 
-app.get("/", (req, res) => res.send("hello"));
-
-//use routes
+//Use routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+app.listen(port, () => console.log(`Server runing on port ${port}`));
